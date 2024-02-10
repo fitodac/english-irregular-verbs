17d57fca608584fdb98f3812557f0d7e93efdda9
fitodac <fitodac@gmail.com>
2024-02-10 11:29:51 -0300

    config files

--- types.d.ts
+++ types.d.ts
@@ -1,4 +1,57 @@
+type settingsType = { [key: string]: string }
+
 type RequestCookie = {
 	key: string
 	value: string
+}
+
+type gameType = {
+	id: number
+	name: string
+	cleared: number
+	stage_id: number
+}
+
+type stageScreenType = {
+	stage: { [key: string]: number }
+	games: any
+}
+
+type levelType = {
+	id: number
+	idx: number
+	game_id: number
+	pairs_id: any
+	sentences_id: any
+}
+
+type translationType = {
+	id: number
+	word: string
+	translation: string
+}
+
+type modeType = {
+	id: number
+	sentence?: string
+	options?: translationType[]
+	word_id?: number
+}
+
+type getLevelType = {
+	level?: levelType
+	game_mode?: modeType
+}
+
+type translationWordType = { id: number; word: string; enabled: boolean }
+
+type translationTranslationType = {
+	id: number
+	translation: string
+	enabled: boolean
+}
+
+type currentGameType = {
+	game: gameType
+	levels: levelType[]
 }
